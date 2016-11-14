02492dab957b09e80ab7cdd1e08c85404e0a9725
rain1024 <brother.rain.1024@gmail.com>
2016-11-14 23:28:28 +0700

    add boiler code

---
+++ src/main/java/week1/percolation/Percolation.java
@@ -0,0 +1,33 @@
+package week1.percolation;
+
+/**
+ * Created by LanAnh on 14/11/2016.
+ */
+public class Percolation {
+   // create n-by-n grid, with all sites blocked
+   public Percolation(int n){}
+
+   // open site (row, col) if it is not open already
+   public void open(int row, int col){}
+
+   // is site (row, col) open?
+   public boolean isOpen(int row, int col){
+      return true;
+
+   }
+
+   // is site (row, col) full?
+   public boolean isFull(int row, int col){
+      return true;
+   }
+
+   // does the system percolate?
+   public boolean percolates(){
+      return true;
+   }
+
+   public static void main(String[] args){ // test client (optional)
+
+   }
+
+}
---
+++ src/main/java/week1/percolation/PercolationStats.java
@@ -0,0 +1,29 @@
+package week1.percolation;
+
+/**
+ * Created by LanAnh on 14/11/2016.
+ */
+public class PercolationStats {
+    public PercolationStats(int n, int trials){}    // perform trials independent experiments on an n-by-n grid
+
+    // sample mean of percolation threshold
+    public double mean(){
+        return 0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+        return 0;
+    }
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo(){
+        return 0;
+    }
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+        return 0;
+    }
+
+    public static void main(String[] args){
+    }
+}
